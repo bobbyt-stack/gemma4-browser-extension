3ab816e8a98eefb030d289f252131b48a69dced3
BT <bobby.taylor82@gmail.com>
2026-05-05 01:13:54 -0400

    Add dist folder for easier loading

---
+++ dist/assets/tools-ITzmStyq.js
@@ -0,0 +1,1 @@
+const _={GET_OPEN_TABS:"get_open_tabs",GO_TO_TAB:"go_to_tab",OPEN_URL:"open_url",CLOSE_TAB:"close_tab",FIND_HISTORY:"find_history",ASK_WEBSITE:"ask_website",HIGHLIGHT_WEBSITE_ELEMENT:"highlight_website_element"};export{_ as A};
